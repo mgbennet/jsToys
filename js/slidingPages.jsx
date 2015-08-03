4488ad649dad1935b5b33e42364d710ab0e1d44c
mgbennet <mgbennet@gmail.com>
2015-08-03 14:59:25 -0700

    Began slidingPages style and jsx files

---
+++ js/slidingPages.jsx
@@ -0,0 +1,18 @@
+var LeftPage = React.createClass({
+	render: function () {
+		return (
+			<div id="leftPage">
+				<div className="rightBtn"></div>
+				<div className="twoColumns">
+					<div id="leftColumn"></div>
+					<div id="rightColumn">
+						<div className="nonesenseLine"></div>
+						<div className="nonesenseLine"></div>
+						<div className="nonesenseLine"></div>
+					</div>
+				</div>
+				<div id="footer"></div>
+			</div>
+		);
+	}
+});
