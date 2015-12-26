18829c73d22993a95cb1266db07098237d92ab41
mgbennet <mgbennet@gmail.com>
2015-12-26 11:56:33 -0700

    Added sliding pages toy functionality.

--- js/slidingPages.jsx
+++ js/slidingPages.jsx
@@ -6,32 +6,98 @@ var DefaultRoute = Router.DefaultRoute,
 	Route = Router.Route,
 	RouteHandler = Router.RouteHandler;
 
-var LeftPage = React.createClass({
-	render: function () {
+var SlidingPages = React.createClass({
+	getInitialState() {
+		return {position: 0, maxPages: 2}
+	},
+	
+	goRight() {
+		if (!this.state.position + 1 >= this.state.maxPages)
+			this.setState({position: this.state.position + 1});
+	},
+	
+	goLeft() {
+		if (!this.state.position - 1 < 0)
+			this.setState({position: this.state.position - 1});
+	},
+	
+	render() {
+		var position = this.state.position,
+			view = position === 0 ? <LeftView /> : <RightView />,
+			btns = [],
+			headerOrb = position === 0 ? "headerOrbLeft" : "headerOrbRight";
+		if (position !== 0) btns.push(
+			<div className="leftBtnWrapper btnWrapper" onClick={this.goLeft}>
+				<div className="leftBtn"></div>
+			</div>
+		);
+		if (position !== this.state.maxPages - 1) btns.push(
+			<div className="rightBtnWrapper btnWrapper" onClick={this.goRight}>
+				<div className="rightBtn"></div>
+			</div>
+		);
 		return (
-			<div id="leftPage">
-				<div className="rightBtnWrapper">
-					<div className="rightBtn"></div>
+			<div id="page">
+				<div id="header">
+					<div id="headerOrb" className={headerOrb}></div>
 				</div>
+				{btns}
+				{view}
+				<div id="footer"></div>
+			</div>
+		)
+	}
+});
+
+var Header = React.createClass({
+	render() {
+		return (
+			<div id="header">
+				<div id="headerOrb" className="headerOrbLeft"></div>
+			</div>
+		);
+	}
+});
+
+var LeftView = React.createClass({
+	render() {
+		var nonesense = [],
+			amountOfNonesense = Math.floor(Math.random() * 5 + 3);
+		for (var i = 0; i < amountOfNonesense; i++) nonesense[i] = <div key={i} className="nonesenseLine"></div>;
+		return (
+			<div id="leftView" className="view">
 				<div className="twoColumns">
-					<div id="leftColumn"></div>
-					<div id="rightColumn">
-						<div className="nonesenseLine"></div>
-						<div className="nonesenseLine"></div>
-						<div className="nonesenseLine"></div>
+					<div className="leftColumn">
+						{nonesense}
+					</div>
+					<div className="rightColumn"></div>
+				</div>
+			</div>
+		);
+	}
+});
+
+var RightView = React.createClass({
+	render() {
+		var nonesense = [],
+			amountOfNonesense = Math.floor(Math.random() * 5 + 3);
+		for (var i = 0; i < amountOfNonesense; i++)
+			nonesense[i] = <div key={i} className="nonesenseLine"></div>;
+		return (
+			<div id="rightView" className="view">
+				<div className="twoColumns">
+					<div className="leftColumn"></div>
+					<div className="rightColumn">
+						{nonesense}
 					</div>
 				</div>
-				<div id="footer"></div>
 			</div>
 		);
 	}
 });
 
 React.render(
-	<div id="page">
-		<div id="header"></div>
-		<LeftPage />
-	</div>,
+	<SlidingPages />,
 	document.getElementById('wrapper')
 );
 
